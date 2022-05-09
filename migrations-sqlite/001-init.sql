620ebadba71f1f1f7ffade997a033cec8d222682
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-05-09 18:33:26 +0100

    small DB update

--- migrations-sqlite/001-init.sql
+++ migrations-sqlite/001-init.sql
@@ -1,13 +1,24 @@
 --up
 
+CREATE TABLE states(
+  id INT PRIMARY KEY,
+  status VARCHAR(20)
+);
+
 CREATE TABLE apps(
   id VARCHAR(36) PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   Link VARCHAR(100) NOT NULL,
   gitLink VARCHAR(100),
-  status VARCHAR(20) NOT NULL
+  status INTEGER,
+  FOREIGN KEY(status) REFERENCES states(id)
 );
 
+INSERT INTO states(id, status) VALUES 
+  (1, 'released'),
+  (2, 'WIP'),
+  (3, 'idea');
+
 INSERT INTO apps(id, name, Link, gitLink, status) VALUES
-  (1, 'Hangman', 'http://hangman.tyway.net', 'https://github.com/howlingunit/hangman', 'released'),
-  (3, 'Old-Tyway', 'http://old.tyway.net', 'https://github.com/howlingunit/old-tyway', 'released');
+  (1, 'Hangman', 'http://hangman.tyway.net', 'https://github.com/howlingunit/hangman', 1),
+  (3, 'Old-Tyway', 'http://old.tyway.net', 'https://github.com/howlingunit/old-tyway', 1);
