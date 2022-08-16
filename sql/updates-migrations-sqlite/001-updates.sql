90ce0ab97413503085e590f9acc52d3929af3bd3
howlingunit <tyler_pitt@outlook.com>
2022-08-17 00:37:28 +0100

    db updates

--- sql/updates-migrations-sqlite/001-updates.sql
+++ sql/updates-migrations-sqlite/001-updates.sql
@@ -20,6 +20,7 @@ INSERT INTO services(id, service) VALUES
   (3, 'STEFS-MASSAGE');
 
 INSERT INTO updates(id, description, Link, date, service) VALUES
+  (4, 'random testimonials', 'http://sms-preview.tyway.net/', '2022-08-17', 3),
   (3, 'typo and styles update', 'http://sms-preview.tyway.net/', '2022-08-12', 3),
   (2, 'fixed navbar sizing', 'http://preview.tyway.net/', '2022-08-09', 1),
   (1, 'Added the updates page', 'http://preview.tyway.net/updates.html', '2022-08-09', 1);
