7e8a24c0ad36f4decf371e786fec256f7e74d625
howlingunit <tyler_pitt@outlook.com>
2022-10-28 20:50:04 +0000

    bug fix

--- sql/updates-migrations-sqlite/001-updates.sql
+++ sql/updates-migrations-sqlite/001-updates.sql
@@ -22,7 +22,7 @@ INSERT INTO services(id, service, link) VALUES
   (3, 'STEFS-MASSAGE', 'https://stefsmassage.co.uk');
 
 INSERT INTO updates(id, description, type, date, service) VALUES
-  (15, 'Added contact information', 'std', '2022-10-28', 1),
+  (16, 'Added contact information', 'std', '2022-10-28', 1),
   (15, 'now using HTTPS', 'std', '2022-10-03', 2),
   (14, 'Released site', 'rel', '2022-09-27', 3),
   (13, 'Released site preview', 'rel', '2022-09-26', 4),
