4bae26d5702fc4747fbbfd75abeb58d4c247c81d
howlingunit <93931085+howlingunit@users.noreply.github.com>
2023-02-20 11:52:53 +0000

    DB update

--- sql/updates-migrations-sqlite/001-updates.sql
+++ sql/updates-migrations-sqlite/001-updates.sql
@@ -23,6 +23,8 @@ INSERT INTO services(id, service, link) VALUES
   (3, 'STEFS-MASSAGE', 'https://stefsmassage.co.uk');
 
 INSERT INTO updates(description, type, date, service) VALUES
+  ('Released site', 'rel', '2023-02-20', 5),
+  ('Updated readme', 'std', '2023-02-20', 5),
   ('Released the 1st version of the frontend for usernames', 'std', '2023-02-13', 5),
   ('Released the API for usernames and have started work on frontend', 'std', '2023-02-10', 5),
   ('Started project "usernames"', 'sta', '2023-02-06', 5),
